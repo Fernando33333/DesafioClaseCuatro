827d4e0af0ccb7a1245332cf0475c584c4324126
Fernando Culetta <fernandoculetta@hotmail.com>
2022-04-21 22:40:40 -0300

    EntregaClase4

---
+++ script.js
@@ -0,0 +1,37 @@
+/*
+let preferencia = parseInt(prompt("Ingrese una preferencia (venta, alquiler, franquicia)").toLowerCase)
+
+if(preferencia === "venta") {
+    console.log("Casas a la venta a partir de u$ 80.000.-")
+} else if (preferencia == "alquiler") {
+    console.log("Casas en alquiler a partir de $40.000.-")
+} else if (preferencia == "franquicia") {
+    console.log("Franquicias de Mcdonalds y Burger king")
+} else {
+    console.log("Ingrese una preferencia valida")
+}
+
+
+let valores = parseInt(prompt("¿Entre que valores busca alquiler? (ingresar numero)"))
+
+
+let valorPropiedad = parseInt(prompt("¿Hasta que valor te buscamos alquiler?"))
+
+for (let valorPropiedad = 10000; valorPropiedad < 50000; valorPropiedad++) { 
+    console.log(valorPropiedad)
+   }
+
+*/
+
+let preferencia = prompt("Ingrese una preferencia (venta, alquiler, franquicia)".toLowerCase)
+while(preferencia === "venta") {
+    console.log("Casas a la venta a partir de u$ 80.000.-")
+}
+while(preferencia == "alquiler") {
+    console.log("Casas en alquiler a partir de $40.000.-")
+}
+while(preferencia == "franquicias") {
+    console.log("Franquicias de Mcdonalds y Burger king")
+}
+
+
